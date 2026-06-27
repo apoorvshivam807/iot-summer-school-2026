c6bcfc460aabb18a09d80ed426e2ce71ce689d11
Apoorv Shivam <apoorvshivam807@gmail.com>
2026-06-27 18:45:38 +0530

    feat: implement smart street light LDR analog threshold control logic

---
+++ week1/smart_street_light/smart_street_light.ino
@@ -0,0 +1,22 @@
+
+
+int LED = 2;
+int LDR = A0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    pinMode(LDR, INPUT);
+}
+
+void loop() {
+    int LDR_value = analogRead(LDR);
+    Serial.println(LDR_value);
+
+    // Turn LED on if ambient light falls below or equals threshold value
+    if (LDR_value <= 300) {
+        digitalWrite(LED, HIGH);
+    } else {
+        digitalWrite(LED, LOW);
+    }
+}
