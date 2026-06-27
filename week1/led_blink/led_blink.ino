3788cbb4581c6a7865a90633efde015b3404f932
Apoorv Shivam <apoorvshivam807@gmail.com>
2026-06-27 15:33:52 +0530

    feat: v1.1 - add serial print tracking for blink count
    Added serial communication to track blink count.

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,5 +1,8 @@
+int blinkCount = 0; // Variable to keep track of the blink count
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600); // Initialize serial communication at 9600 bps
 }
 
 void loop() {
@@ -7,4 +10,8 @@ void loop() {
   delay(500);
   digitalWrite(13, LOW);
   delay(500);
+  
+  blinkCount++; // Increment the counter
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount); // Print the current count to the Serial Monitor
 }
