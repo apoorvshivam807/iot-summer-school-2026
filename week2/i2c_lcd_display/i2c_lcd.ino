7b72d852b7e960e83c6c938d304f76ee7a7beec3
Apoorv Shivam <apoorvshivam807@gmail.com>
2026-06-28 13:26:10 +0530

    feat: implement I2C liquid crystal display initialization and hello world print

---
+++ week2/i2c_lcd_display/i2c_lcd.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_LiquidCrystal.h>
+
+// Initialize the LCD with the I2C address 0 (default for adapter chipsets)
+Adafruit_LiquidCrystal lcd_1(0);
+
+void setup()
+{
+  // Setup the LCD's number of columns and rows:
+  lcd_1.begin(16, 2);
+
+  // Print our welcome string message to the screen layout
+  lcd_1.print("hello world");
+  delay(10000); 
+}
+
+void loop() {
+  delay(10); // Low overhead background delay loop
+}
